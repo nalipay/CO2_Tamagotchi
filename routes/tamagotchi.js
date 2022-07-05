9e2fad2845026f9cab4113c1c3f808d2a3ec58da
Jule <bingthemachine@gmail.com>
2022-07-05 13:57:19 +0200

    tamagotchi create route with link to user id

--- routes/tamagotchi.js
+++ routes/tamagotchi.js
@@ -4,35 +4,29 @@ const bcryptjs = require("bcrypt");
 const mongoose = require("mongoose");
 
 const Tamagotchi = require('../models/Tamagotchi.model')
+const User = require('../models/User.model')
 
 const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
 
 
 // Tamagotchi create
+router.get("/", isLoggedIn, (req, res, next) => {
+    res.render("user", { userInSession: req.session.currentUser },)
+})
+
 
 router.post('/create', (req, res, next) => {
-    const { username, password } = req.body
+    const userId = req.session.currentUser._id
+    const { avatarName } = req.body
+    console.log(userId)
+    Tamagotchi.create({ avatarName: avatarName, user: userId })
+        .then(createdAvatar => {
+            console.log(createdAvatar, userId)
 
-
-    User.findOne({ username: username })
-        .then(userFromDB => {
-            if (userFromDB !== null) {
-                res.render('index', { signinMessage: 'Username is already taken' })
-            } else {
-                // and hash the password
-                const salt = bcryptjs.genSaltSync()
-                const hash = bcryptjs.hashSync(password, salt)
-                // create the user
-                User.create({ username, password: hash })
-                    .then(createdUser => {
-                        console.log(createdUser)
-                        // if we want to log the user in using passport
-                        // req.login()
-                        res.redirect('/user')
-                    })
-                    .catch(err => next(err))
-            }
+            res.redirect('/user')
         })
+        .catch(err => next(err))
+
 });
 
 
